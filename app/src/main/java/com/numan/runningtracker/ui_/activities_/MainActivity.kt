271e47d063bcde72c611af87833d8d8755c8df94
Numan <flopy6663@gmail.com>
2020-07-20 18:48:17 +0500

    Setted Navigation Between Fragments

--- app/src/main/java/com/numan/runningtracker/ui_/activities_/MainActivity.kt
+++ app/src/main/java/com/numan/runningtracker/ui_/activities_/MainActivity.kt
@@ -2,18 +2,41 @@ package com.numan.runningtracker.ui_.activities_
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.numan.runningtracker.R
 import com.numan.runningtracker.db_.dao_.RunDao
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var runDao: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       println("ffnet: Run Dao: ${runDao.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        /*
+        * Since we only want to show our bottom navigation in statistics, Run and Setting Fragment
+        *
+        * And we don't want to show in setup and Tracking Fragment
+        * For this purpose we'll use onDestination Change listener
+        * */
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+
+                }
+            }
     }
 }
--- app/src/main/java/com/numan/runningtracker/ui_/fragments_/RunFragment.kt
+++ app/src/main/java/com/numan/runningtracker/ui_/fragments_/RunFragment.kt
@@ -1,10 +1,14 @@
 package com.numan.runningtracker.ui_.fragments_
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.numan.runningtracker.R
 import com.numan.runningtracker.ui_.viewmodels_.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 /*
 * Whenever we want to inject stuff in Android Components
@@ -18,5 +22,12 @@ class RunFragment : Fragment(R.layout.fragment_run) {
     * So,
     * */
     val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 
 }
