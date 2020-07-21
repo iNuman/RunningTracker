3ce1abf02d18cd1b1c6a96f9e98e005cc509f506
Numan <flopy6663@gmail.com>
2020-07-21 18:04:32 +0500

    Setting Up Permissions

--- app/src/main/java/com/numan/runningtracker/other_/Constants.kt
+++ app/src/main/java/com/numan/runningtracker/other_/Constants.kt
@@ -2,4 +2,5 @@ package com.numan.runningtracker.other_
 
 object Constants {
    const val RUNNING_DATABASE_NAME = "running_db"
+   const val REQUEST_CODE_LOCATION_PERMISSION = 0
 }
--- app/src/main/java/com/numan/runningtracker/ui_/fragments_/RunFragment.kt
+++ app/src/main/java/com/numan/runningtracker/ui_/fragments_/RunFragment.kt
@@ -1,21 +1,27 @@
 package com.numan.runningtracker.ui_.fragments_
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.numan.runningtracker.R
+import com.numan.runningtracker.other_.Constants.REQUEST_CODE_LOCATION_PERMISSION
 import com.numan.runningtracker.ui_.viewmodels_.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import pub.devrel.easypermissions.EasyPermissions.hasPermissions
 
 /*
 * Whenever we want to inject stuff in Android Components
 * we will Annotate that fragment/Activity with:
 * */
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
     /*
     * We can't just inject view model because we have to specify
     * which view model will be injected here
@@ -25,9 +31,60 @@ class RunFragment : Fragment(R.layout.fragment_run) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requestPermission()
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun requestPermission() {
+        if (hasPermissions(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this@RunFragment,
+                "You need to accept the Location Permissions",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this@RunFragment,
+                "You need to accept the Location Permissions",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        /*
+        * Here perm contain the list of permissions that has been denied
+        * */
+        if (EasyPermissions.somePermissionPermanentlyDenied(this@RunFragment, perms)) {
+            AppSettingsDialog.Builder(this@RunFragment).build().show()
+        } else requestPermission()
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults,
+            this@RunFragment
+        )
     }
 
 }
