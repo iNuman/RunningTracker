475f109997d4b09c2418200432eb00e2bcaba0d7
Numan <flopy6663@gmail.com>
2020-07-22 23:42:16 +0500

    Added a Foreground Service and pending Intent setup Logic done

--- app/src/main/java/com/numan/runningtracker/BaseApplication.kt
+++ app/src/main/java/com/numan/runningtracker/BaseApplication.kt
@@ -1,13 +1,36 @@
 package com.numan.runningtracker
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.numan.runningtracker.other_.Constants
+import com.numan.runningtracker.other_.Constants.NOTIFICATION_CHANNEL_ID
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-
+@RequiresApi(Build.VERSION_CODES.O)
 @HiltAndroidApp
 class BaseApplication: Application() {
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        createNotificationChannel()
+    }
+
+
+    private fun createNotificationChannel() {
+            val name = applicationContext.getString(R.string.app_name)
+            val channelName = Constants.NOTIFICATION_CHANNEL_NAME
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply {
+                description = channelName
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
     }
 }
--- app/src/main/java/com/numan/runningtracker/services_/TrackingServices.kt
+++ app/src/main/java/com/numan/runningtracker/services_/TrackingServices.kt
@@ -1,28 +1,62 @@
 package com.numan.runningtracker.services_
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.numan.runningtracker.R
+import com.numan.runningtracker.ui_.activities_.MainActivity
 import timber.log.Timber
 import java.util.*
 import com.numan.runningtracker.other_.Constants as Constants
 
-class TrackingServices : LifecycleService(){
+class TrackingServices : LifecycleService() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
-            when (it.action){
+            when (it.action) {
 
-                Constants.ACTION_START_OR_RESUME_SERVICE ->{
+                Constants.ACTION_START_OR_RESUME_SERVICE -> {
                     Timber.d("Started or Resume Service")
                 }
-                Constants.ACTION_PAUSE_SERVICE ->{
+                Constants.ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused Service")
                 }
-                Constants.ACTION_STOP_SERVICE ->{
+                Constants.ACTION_STOP_SERVICE -> {
                     Timber.d("Stopped Service")
                 }
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    /*
+    * So in here what we actually doing is
+    * Whenever we get main activity we check for the action attach to it
+    * if it is then we'll simply navigate to that tracking fragment
+    * */
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this@TrackingServices,
+        0,
+        Intent(this@TrackingServices, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    private fun startForegroundService() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationBuilder =
+            NotificationCompat.Builder(this@TrackingServices, Constants.NOTIFICATION_CHANNEL_ID)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+                .setContentText(getString(R.string.app_name))
+                .setContentText("00:00:00")
     }
 }
 
